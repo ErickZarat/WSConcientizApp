6c28359c09b1ac39a9816a158674f85f2d94f451
ErickZarat <ezarar15@gmail.com>
2016-09-03 23:54:02 -0600

    problema agregado

---
+++ controller/ControladorProblema.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Zarathos on 03/09/2016.
+ */
+module.exports = function (modelo) {
+    return{
+        add:function (req, res) {
+            modelo.problema.create(req.body).then(function () {
+                res.json({"mensaje": "Problema agregado"});
+            }).error(function (err) {
+                res.json({"mensaje": "El problema no se pudo agregar, si esto continua comuniquese con el administrador."});
+                throw err;
+            });
+        },
+        list:function (req, res) {
+            modelo.prueba.findAll({
+                where: {
+                    id_problema:req.params.id
+                }
+            }).then(function (data) {
+                res.json(data);
+            }).error(function () {
+                res.json({"mensaje": "Error al listar problemas", "status": 500});
+            });
+        }
+    }
+};
